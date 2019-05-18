72ae75db071d714f1e795640f093081ef3dd6b01
guilhermejunkes <guilhermepjunkes@gmail.com>
2019-05-18 16:26:47 -0300

    Cadastro de pessoas

---
+++ Servicos/Data/ServicosContexto.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Servicos.Models;
+
+namespace Servicos.Data
+{
+    public class ServicosContexto : DbContext
+    {
+        public ServicosContexto()
+            : base("ServicosContextoDev")
+        {
+
+        }
+
+        public DbSet<Pessoa> Pessoa { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Servicos/Models/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicos.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NomeFantasia { get; set; }
+        public string Sexo { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public int TipoPessoa { get; set; }
+
+
+        //id, nome, nome fantasia, sexo, telefone, email, 
+        //tipo pessoa(Fisica, Juridica, Exportacao - pode ser um passaporte)
+    }
+}
